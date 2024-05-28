f2304d2f78921878efec80e4fa83651b495c34fa
Evan Wies <evan@neomantra.net>
2024-05-28 17:05:54 -0400

    Fixup hist testing namespace

--- hist/hist_test.go
+++ hist/hist_test.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2024 Neomantra Corp
 
-package dbn_hist_test
+package dbn_hist
 
 import (
 	"os"
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	dbn "github.com/NimbleMarkets/dbn-go"
-	dbn_hist "github.com/NimbleMarkets/dbn-go/hist"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -31,42 +31,42 @@ var _ = BeforeSuite(func() {
 var _ = Describe("DbnHist", func() {
 	Context("metadata", func() {
 		It("should ListPublishers", func() {
-			publishers, err := dbn_hist.ListPublishers(databentoApiKey)
+			publishers, err := ListPublishers(databentoApiKey)
 			Expect(err).To(BeNil())
 			Expect(publishers).ToNot(BeEmpty())
 		})
 		It("should ListDatasets, ListSchemas, ListFields, ListUnitPrices", func() {
-			datasets, err := dbn_hist.ListDatasets(databentoApiKey, dbn_hist.DateRange{})
+			datasets, err := ListDatasets(databentoApiKey, DateRange{})
 			Expect(err).To(BeNil())
 			Expect(datasets).ToNot(BeEmpty())
 
-			schemas, err := dbn_hist.ListSchemas(databentoApiKey, datasets[0])
+			schemas, err := ListSchemas(databentoApiKey, datasets[0])
 			Expect(err).To(BeNil())
 			Expect(schemas).ToNot(BeEmpty())
 
 			schema, err := dbn.SchemaFromString(schemas[0])
 			Expect(err).To(BeNil())
 
-			fields, err := dbn_hist.ListFields(databentoApiKey, dbn.Encoding_Dbn, schema)
+			fields, err := ListFields(databentoApiKey, dbn.Encoding_Dbn, schema)
 			Expect(err).To(BeNil())
 			Expect(fields).ToNot(BeEmpty())
 
-			unitPrices, err := dbn_hist.ListUnitPrices(databentoApiKey, datasets[0])
+			unitPrices, err := ListUnitPrices(databentoApiKey, datasets[0])
 			Expect(err).To(BeNil())
 			Expect(unitPrices).ToNot(BeEmpty())
 		})
 		It("should ResolveSymbology", func() {
-			resolveParams := dbn_hist.ResolveParams{
+			resolveParams := ResolveParams{
 				Dataset:  "XNAS.ITCH",
 				Symbols:  []string{"AAPL"},
 				StypeIn:  dbn.SType_RawSymbol,
 				StypeOut: dbn.SType_InstrumentId,
-				DateRange: dbn_hist.DateRange{
+				DateRange: DateRange{
 					Start: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 					End:   time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
 				},
 			}
-			resolveResp, err := dbn_hist.SymbologyResolve(databentoApiKey, resolveParams)
+			resolveResp, err := SymbologyResolve(databentoApiKey, resolveParams)
 			Expect(err).To(BeNil())
 			Expect(resolveResp).ToNot(BeNil())
 			Expect(resolveResp.Mappings).ToNot(BeEmpty())
