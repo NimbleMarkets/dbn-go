b3f050bc4dae0449fe0a3867e705c38d58b69256
Evan Wies <evan@neomantra.net>
2024-05-28 21:05:12 -0400

    Add NullVisitor template

---
+++ null_visitor.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2024 Neomantra Corp
+
+package dbn
+
+// NullVisitor is an implementation of all the dbn.Visitor interface.
+// It is useful for copy/pasting to ones own implementation.
+type NullVisitor struct {
+}
+
+func (v *NullVisitor) OnMbp0(record *Mbp0) error {
+	return nil
+}
+
+func (v *NullVisitor) OnMbp10(record *Mbp10Msg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnMbp1(record *Mbp1Msg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnMbo(record *MboMsg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnOhlcv(record *Ohlcv) error {
+	return nil
+}
+
+func (v *NullVisitor) OnImbalance(record *Imbalance) error {
+	return nil
+}
+
+func (v *NullVisitor) OnStatMsg(record *StatMsg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnErrorMsg(record *ErrorMsg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnSystemMsg(record *SystemMsg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnSymbolMappingMsg(record *SymbolMappingMsg) error {
+	return nil
+}
+
+func (v *NullVisitor) OnStreamEnd() error {
+	return nil
+}
